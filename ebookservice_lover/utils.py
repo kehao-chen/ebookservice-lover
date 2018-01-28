9f8886d16c17c16342593d356c2dfd08c47fb2c8
Kehao Chen <kehao.chen@happyhacking.ninja>
2018-01-28 16:15:24 +0800

    perf: to replace the requests module with aiohttp

--- ebookservice_lover/utils.py
+++ ebookservice_lover/utils.py
@@ -1,20 +1,32 @@
 # -*- coding: utf-8 -*-
 """This module provides utility functions.
 """
-import requests
+import aiohttp
+import asyncio
+
+CHUNK_SIZE = 1024
 
 
-def download_file(url: str, path: str) -> bool:
+async def download_file(
+        file_url: str,
+        semaphore: asyncio.Semaphore,
+        session: aiohttp.ClientSession,
+        path: str) -> bool:
     """Download content from URL and save it to target path
 
-    :param url: URL of content.
+    :param file_url: URL of content.
+    :param semaphore:
+    :param session:
     :param path: Target path for saving file.
     :return: bool
     """
-    r = requests.get(url, stream=True)
-    with open(path, 'wb') as f:
-        for chunk in r.iter_content(chunk_size=1024):
-            if chunk:  # filter out keep-alive new chunks
-                f.write(chunk)
-                f.flush()
+    with await semaphore:
+        with open(path, 'wb') as file:
+            async with session.get(file_url) as response:
+                while True:
+                    chunk = await response.content.read(CHUNK_SIZE)
+                    if not chunk:
+                        break
+                    file.write(chunk)
+                    file.flush()
     return True
